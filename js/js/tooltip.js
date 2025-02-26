2f62cdcb979a181ac47bcf684a087275a1348f01
RX580-Oficial <GhostSilva544@gmail.com>
2025-02-26 09:36:12 -0300

    Delete js directory

--- js/js/tooltip.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const games = Array.from(document.getElementsByClassName('game'));
-
-  games.forEach(game => {
-    const img = game.querySelector('img');
-    const title = game.querySelector('span').textContent;
-    img.setAttribute('title', title);
-  });
-});
