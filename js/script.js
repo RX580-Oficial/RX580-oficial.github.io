caf7d3a2c11dc05e0fc19d6905403929e8016e87
RX580-Oficial <GhostSilva544@gmail.com>
2025-03-06 23:15:47 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@
 let flameContainer = document.createElement('div');
 flameContainer.innerHTML = `
   <div style="position:relative;">
-    <img src="ruta_a_tu_imagen_de_llama.png" alt="Llama" style="width:100px; height:100px; position:absolute; top:-50px;">
+    <img src="llama.png" alt="Llama" style="width:100px; height:100px; position:absolute; top:-50px;">
     <span style="font-size:20px; color:red; font-weight:bold; position:absolute; top:60px; left:0;">Popular</span>
   </div>
 `;
 
-// Obtener el elemento del juego m·s descargado
+// Obtener el elemento del juego m√°s descargado
 let mostDownloadedGame = document.querySelector('#id_del_juego_mas_descargado');
 
-// AÒadir el contenedor de la llama y el texto encima del juego m·s descargado
+// A√±adir el contenedor de la llama y el texto encima del juego m√°s descargado
 mostDownloadedGame.appendChild(flameContainer);
