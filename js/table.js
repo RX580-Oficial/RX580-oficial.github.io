2b90d10b25ce5fd51d9b3fea5edc1725afbb142a
RX580-Oficial <GhostSilva544@gmail.com>
2025-07-09 15:39:33 -0300

    Add files via upload

--- js/table.js
+++ js/table.js
@@ -3,17 +3,15 @@ const boxes = document.querySelectorAll('.box');
 boxes.forEach(box => {
   box.addEventListener('click', () => {
     if (box.id === 'games') {
-      // Abre la página de juegos
-      window.open('index.html', '_blank');
+      window.open('index.html', '');
+    } else if (box.id === 'cracks') {
+      window.open('Cracks.html', '');
     } else if (box.id === 'videos') {
-      // Abre la página de videos
-      window.open('mis_videos.html', '_blank');
-    } else if (box.id === 'iniciar') {
-      // Abre la página de videos
-      window.open('Inicio.html', '_blank');
-    } else if (box.id === 'donar') {
-      // Pagina de pagos
-     window.open('https://www.paypal.me/proplayerpay', '_blank');
+      window.open('Inicio.html', '');
+    } else if (box.id === 'comentarios') {
+      window.open('Comentarios.html', '');
+    } else if (box.id === 'version') {
+      window.open('index.html#', '');
     }
-  });
+  }); // ? Este paréntesis estaba perdido como yo los lunes sin café
 });
